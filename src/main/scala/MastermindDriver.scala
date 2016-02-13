4693bf668a4a58bbbb8685ad6fabcbfdd32df2d5
James <jamesghill@yahoo.co.uk>
2016-02-13 20:25:49 +0000

    Added flow control to create 'easy' or 'hard' games.

--- src/main/scala/MastermindDriver.scala
+++ src/main/scala/MastermindDriver.scala
@@ -1,11 +1,28 @@
 object MastermindDriver {
   
+  val errorMessage = "That is not a valid command."
+  val easy = "E"
+  
   def main(args: Array[String]) {
     
-    var g: Game = Factory.getInstance(classOf[Game], true)
-    g.runGames
-    g = Factory.getInstance(classOf[Game], false)
-    g.runGames
+    if(args(1).equals(easy)){
+      
+      // Run easy game 
+      var g: Game = Factory.getInstance(classOf[Game], true)
+      g.runGames
+      
+    } else if (args.isEmpty) {
+      
+      // Run hard game
+      var g: Game = Factory.getInstance(classOf[Game], false)
+      g.runGames
+      
+    } else {
+      
+      // Show an error message
+      println(errorMessage)
+      
+    }
     
   }
   
