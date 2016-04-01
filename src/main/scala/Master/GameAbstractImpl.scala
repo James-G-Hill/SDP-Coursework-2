1f72786ad19ab1731623509f5a82e9fd50cd7431
James <jamesghill@yahoo.co.uk>
2016-04-01 19:41:54 +0100

    Created Master package and pulled all relevant files into it.

---
+++ src/main/scala/Master/GameAbstractImpl.scala
@@ -0,0 +1,29 @@
+package Master
+
+abstract class GameAbstractImpl extends Game {
+  
+  private var showCode: Boolean = false
+  
+  /**
+    * Create a Game object.
+    *
+    * @param easy If easy is true the secret code will be
+    *             revealed at all times when playing the game. If easy is
+    *             false the secret code is not revealed until correctly guessed
+    *             or the player runs out of turns.
+    *             
+    */
+  def this(easy: Boolean) {
+    
+    this()
+    showCode = easy
+  
+  }
+  
+}
+
+class NewGame(b: Boolean) extends GameAbstractImpl {
+  
+  
+  
+}
