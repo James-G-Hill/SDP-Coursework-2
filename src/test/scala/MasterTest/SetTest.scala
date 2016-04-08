164064cb4f3823496e55e5225c8438ffcb64f904
James <jamesghill@yahoo.co.uk>
2016-04-08 21:37:44 +0100

    Transformed SetTest to scalatest format.

--- src/test/scala/MasterTest/SetTest.scala
+++ src/test/scala/MasterTest/SetTest.scala
@@ -1,30 +1,17 @@
-import Traits.AbstractSet
-import Traits.ColourSet
-
 import org.junit._
 import org.junit.runner._
 import org.scalatest.junit._
 import org.scalatest._
+import org.scalatest.FunSuite
 import Assert._
 
-class SetTest {
+class SetTest extends FunSuite {
 
   val size = 4
   
-  @Before
-  def setUp: Unit = {
-  }
-
-  @After
-  def tearDown: Unit = {
-  }
-
-  @Test
-  def createColourSet = {
-    
+  test("A new ColourSet should be an instance of AbstractSet"){
     val cols = new ColourSet("ABCD")
     assert(cols.isInstanceOf[AbstractSet])
-    
   }
-
+  
 }
