22b9a28bde39979e443b9a6523f09a93d79947ac
James <jamesghill@yahoo.co.uk>
2016-04-09 21:20:47 +0100

    Refactored classes.

--- src/main/scala/Traits/Board.scala
+++ src/main/scala/Traits/Board.scala
@@ -1,34 +1,27 @@
 package Traits
 
 /**
-@return board: the game board
-  **/
+  @return board: the game board
+**/
 trait Board {
 
   val size = 12
-
-  def colourArray : Array[Guesses]
-
-  def pegArray : Array[PegSet]
-
-  def getBoard()
-
+  
   def addRow()
-
-  var turn = 0
-
+  def getBoard()
+  def colourArray : Array[Guesses]
+  def pegArray : Array[PegSet]
+  
 }
 
 class BoardImpl extends Board {
-
-  def colourArray: Array[Guesses] = {new Array[Guesses](1)}
-
-  def pegArray: Array[PegSet] = {new Array[PegSet](1)}
-
+  
+  def addRow() = {}
+  
   def getBoard() = {}
-
-  def addRow() = {
-    turn = turn+1
-  }
-
+  
+  def colourArray: Array[Guesses] = {new Array[Guesses](1)}
+  
+  def pegArray: Array[PegSet] = {new Array[PegSet](1)}
+  
 }
