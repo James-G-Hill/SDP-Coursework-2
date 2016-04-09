8a587178f4826d0584274effa7781a07fcbeaee2
James <jamesghill@yahoo.co.uk>
2016-04-09 18:20:37 +0100

    Added a 'theCode' to generate the secret code.

--- src/main/scala/Traits/SecretCode.scala
+++ src/main/scala/Traits/SecretCode.scala
@@ -1,7 +1,32 @@
 package Traits
 
-trait SecretCode extends ColourSet {
+import scala.util.Random
 
-  def secretCode: ColourSet
+trait SecretCode {
 
+  def getCode: ColourSet
+
+}
+
+class SecretCodeImpl(codeSize: Int) extends SecretCode {
+  
+  val theCode: ColourSet = {
+    
+    var s = ""
+    val vc = ValidColoursImpl.allColours
+    
+    var a = 0
+    for(a <- 1 to codeSize) {
+      
+      val r = new Random(System.currentTimeMillis())
+      val index = r.nextInt(vc.length)
+      s = s + vc(index).code
+    }
+    
+    new ColourSet(s)
+    
+  }
+  
+  override def getCode: ColourSet = {theCode}
+  
 }
