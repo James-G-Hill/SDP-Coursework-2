2c6c4d62837769580809dcf34e195e17c400eb68
James <jamesghill@yahoo.co.uk>
2016-04-10 18:13:25 +0100

    Added in getBeanFactory, attempted to modify getInstance.

--- src/main/scala/Master/Factory.scala
+++ src/main/scala/Master/Factory.scala
@@ -2,15 +2,25 @@ package Master
 
 import Traits.Board
 import Traits.BoardImpl
+import org.springframework.beans.factory.BeanFactory
+import org.springframework.context.support.ClassPathXmlApplicationContext
 
 object Factory {
 
-  def getInstance(c: Class[_], b: Boolean): Game = {
+  def getInstance(c: Class[_], b: Boolean) = {
     new NewGame(b)
+    //val cons = c.getConstructor(b.getClass)
+    //cons.setAccessible(true)
+    //cons.newInstance(b)
   }
 
   def getInstanceBoard(c: Class[_]): Board = {
     new BoardImpl
   }
-
+  
+  def getBeanFactory = {
+    val factory: BeanFactory = new ClassPathXmlApplicationContext("file:src/main/validColours.xml")
+    factory
+  }
+  
 }
