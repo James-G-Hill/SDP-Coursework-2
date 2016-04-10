c5daa782c9b28c1490f421993ffd9678600d4824
James <jamesghill@yahoo.co.uk>
2016-04-10 18:17:26 +0100

    Created code that creates colours from Spring beans file.

--- src/main/scala/Traits/ValidColours.scala
+++ src/main/scala/Traits/ValidColours.scala
@@ -1,6 +1,7 @@
 package Traits
 
 import Classes.Colours
+import Master.Factory
 
 trait ValidColours {
 
@@ -12,8 +13,23 @@ object ValidColoursImpl extends ValidColours {
   
   val allColours: Array[Colours] = {
     
-    new Array[Colours](1)
+    val codes: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val cols = new Array[Colours](6)
+    val factory = Factory.getBeanFactory
     
+    for(i <- 0 to codes.length) {
+      try {
+        val colour = factory.getBean(codes.charAt(i).toString).asInstanceOf[Colours]
+        cols(i) = colour
+      }
+      catch{case e: Exception => }
+    }
+    
+    cols
+  }
+  
+  def getColours(char: Char): Colours = {
+    allColours(allColours.indexOf(char))
   }
   
 }
