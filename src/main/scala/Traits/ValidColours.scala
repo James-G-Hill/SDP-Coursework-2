fa2354c46432f18b20ee9b11f5fbc1562251603e
James <jamesghill@yahoo.co.uk>
2016-04-10 19:10:59 +0100

    Changed allColours type to HashMap.

--- src/main/scala/Traits/ValidColours.scala
+++ src/main/scala/Traits/ValidColours.scala
@@ -2,34 +2,31 @@ package Traits
 
 import Classes.Colours
 import Master.Factory
+import java.util.HashMap
 
-trait ValidColours {
-
-  def allColours: Array[Colours]
-
-}
+trait ValidColours {}
 
 object ValidColoursImpl extends ValidColours {
   
-  val allColours: Array[Colours] = {
+  val allColours: HashMap[String, Colours] = {
     
     val codes: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    val cols = new Array[Colours](6)
+    val cols = new HashMap[String, Colours]()
     val factory = Factory.getBeanFactory
     
     for(i <- 0 to codes.length) {
       try {
         val colour = factory.getBean(codes.charAt(i).toString).asInstanceOf[Colours]
-        cols(i) = colour
+        cols.put(colour.code.toString, colour)
       }
       catch{case e: Exception => }
     }
-    
     cols
   }
   
   def getColours(char: Char): Colours = {
-    allColours(allColours.indexOf(char))
+    println("index is " + char)
+    allColours get char.toString
   }
   
 }
