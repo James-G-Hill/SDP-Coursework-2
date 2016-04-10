71224a21181d17d4824347d486bed82a8569390d
James <jamesghill@yahoo.co.uk>
2016-04-10 19:44:51 +0100

    Minor changes to the formatting of text.

--- src/main/scala/Master/GamePrinter.scala
+++ src/main/scala/Master/GamePrinter.scala
@@ -8,35 +8,35 @@ class GamePrinter(codesize: Int, validColours: ValidColours) {
   
   def welcome =
     "Welcome to Mastermind.\n\n" +
-      "This is a text version of the classic board game Mastermind.\n" +
+      "This is a text version of the classic board game Mastermind.\n\n" +
       "The computer will think of a secret code.\n" +
       "The code consists of " + codesize + " colored pegs.\n" +
       "The pegs may be one of " + validColours.allColours.size + " colors:" + colourList + "\n" +
       "A color may appear more than once in the code.\n\n" +
-      "You try to guess what colored pegs are in the code and what order they are in\n" +
+      "You try to guess what colored pegs are in the code and what order they are in.\n" +
       "After making a guess the result will be displayed.\n" +
       "A result consists of a black peg for each peg " +
       "you have exactly correct (color and position) in your guess.\n" +
       "For each peg in the guess that is the correct color, but is out of position, you get a white peg.\n\n" +
       "Only the first letter of the color is displayed. B for Blue, R for Red, and so forth.\n" +
-      "When entering guesses you only need to enter the first character of the color as a capital letter.\n" +
+      "When entering guesses you only need to enter the first character of the color as a capital letter.\n\n" +
       "You have 12 to guess the answer or you lose the game. \n" +
-      "Generating secret code ....\n"
+      "Generating secret code ....\n\n"
       
   def colourList = {
     var s = ""
-    
     for(colour: Colours <- validColours.allColours.values){
       s = s + " " + colour.desc + ","
     }
+    s
   }
 
   def secretPrint(code:String) = {
-    "The secret code is " + code
+    "The secret code is " + code + ".\n\n"
   }
 
   def guessPrint(n: Int) = {
-    "You have " + n + " guesses left"
+    "You have " + n + " guesses left . . .\n"
   }
 
   def getGuess = {
